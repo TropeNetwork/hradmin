19da8cad1c65a51a6b2920cc8934905b8f396705
cbleek <cbleek>
2005-04-22 07:11:46 +0000

    *** empty log message ***

--- public_html/group.php
+++ public_html/group.php
@@ -18,10 +18,10 @@
  *
  *   Author: Gerrit Goetsch <goetsch@cross-solution.de>
  *   
- *   $Id: group.php,v 1.8 2005/04/21 15:44:02 cbleek Exp $
+ *   $Id: group.php,v 1.9 2005/04/22 07:11:46 cbleek Exp $
  */
 require_once 'HTML/QuickForm.php';
-require_once 'HTML/QuickForm/Renderer/ITStatic.php';
+require_once 'HTML/QuickForm/Renderer/ITDynamic.php';
 
 include_once 'common.inc';
 
@@ -42,10 +42,13 @@ $tpl->setVariable(array('maxlength'=>'100',
 
 $form->addElement('text', 'define', _("Define name"));
 
-$tpl->setVariable(array('maxlength'=>'15',
-                  'class'=>'formFieldLong'));
+$tpl->setVariable(array('maxlength' => '15',
+                        'class'     => 'formFieldLong'));
 
 if ($edit) {
+
+    $form->addElement('hidden', 'id', $_GET['edit']);
+
     if ($level>1) {
         $form->addElement('submit', 'submit', _("Save"));
     }
@@ -62,19 +65,19 @@ if ($edit) {
     $defaultValues['name']          = $groups[0]['name'];
     $defaultValues['define']        = $groups[0]['group_define_name'];
     $defaultValues['description']   = $groups[0]['description'];
-    
-    $params = array(
-        'fields'  => array('right_id'),
-        'filters' => array('group_id' => $_GET['edit'])
-                  );
+    $defaultValues['id']            = $_GET['edit'];
+
+
+    $params = array('fields'  => array('right_id','right_level'),
+                    'filters' => array('group_id' => $_GET['edit']));
                   
     $group_rights = $admin->perm->getRights($params);
-    
-    var_dump::display($group_rights);
-    
-    $form->addElement('hidden', 'id', $_GET['edit']);
-    
+    foreach ($group_rights AS $right){
+       $selectedRights[$right['right_id']]=$right['right_level'];      
+    }
+    $defaultValues['rights'] = $selectedRights;   
     $form->setDefaults($defaultValues);
+    
 } else {
     $form->addElement('submit', 'submit', _("Create"));
 }
@@ -99,14 +102,22 @@ foreach($apps as $app) {
     foreach($areas as $area) {
         $rights = $admin->perm->getRights(array('filters' => array('application_id' => $app['application_id'],
                                                                    'area_id'        => $area['area_id'] ),
-                                                'fields' => array('name','right_id')));
+                                                'fields'  => array('name','right_id')));
         foreach($rights as $right) {
             $tpl->setVariable(array('application'   => $app['name'],
                                     'area'          => $area['name'],
-                                    'right'         => $right['name'],
-                                    'right_box'     => getRightLevelBox($right['right_id'],$group_rights)));
+                                    'right'         => $right['name']));
+
+            $selectElements[]=HTML_QuickForm::createElement('select',$right['right_id'],null,array('0'=>HRADMIN_LEVEL_0,
+                                                                                                   '1'=>HRADMIN_LEVEL_1,
+                                                                                                   '2'=>HRADMIN_LEVEL_2,
+                                                                                                   '3'=>HRADMIN_LEVEL_3));
+                                                                                          
+          
             $tpl->parseCurrentBlock();
         }
+        $form->addGroup(@$selectElements,'rights');
+
     }
 }
 
@@ -117,17 +128,21 @@ if ($level < 2) {
 
 if ($form->validate()) {
     if ($delete) {
-        $admin->perm->removeGroup(array('group_id' => $form->exportValue('id'),
+        $admin->perm->removeGroup(array('group_id' => $_POST['id'],
                                         'recursive' => true ));
         header("Location: groups.php");
-    } elseif ( $form->exportValue('id') >0 && $level>1) {
+    } elseif ( @$_POST['id'] >0 && $level>1) {
+    print "UPDATE";
+    
+    $values=$form->exportValues();
+    var_dump::display($values);
 
         $data   = array('group_define_name'=>$form->exportValue("define"));
-        $filter = array('group_id'=>$form->exportValue('id'));
+        $filter = array('group_id'=> $_POST['id']);
         $admin->perm->updateGroup($data,$filter);
         
         
-        $filter = array('section_id'   => $form->exportValue('id'),
+        $filter = array('section_id'   => $_POST['id'],
                         'section_type' => LIVEUSER_SECTION_GROUP, 
                         'language_id'  =>0);
         $data   = array('name'         => $form->exportValue("name"), 
@@ -135,8 +150,7 @@ if ($form->validate()) {
                 
         $admin->perm->updateTranslation($data,$filter);
                     
-        
-        setGroupRights($form->exportValue('id'),$form->exportValue('rights'));
+        setGroupRights($_POST['id'],$_POST['rights']);
         header("Location: groups.php");
     } elseif ($level>2) {  
         $group_id = $admin->perm->addGroup(
@@ -157,9 +171,15 @@ if ($form->validate()) {
     exit;
 }
 
-$renderer =& new HTML_QuickForm_Renderer_ITStatic($tpl);
-$renderer->setRequiredTemplate('{label}<font color="red" size="1">*</font>');
-$renderer->setErrorTemplate('<font color="orange" size="1">{error}</font><br/>{html}');            
+$renderer =& new HTML_QuickForm_Renderer_ITDynamic($tpl);
+#$renderer->setRequiredTemplate('{label}<font color="red" size="1">*</font>');
+#$renderer->setErrorTemplate('<font color="orange" size="1">{error}</font><br/>{html}');            
+
+$renderer->setElementBlock(array(
+    'name'     => 'qf_group_table',
+    'address'  => 'qf_group_table'
+));
+        
 
 $form->accept($renderer);
 
@@ -168,43 +188,45 @@ $tpl->setVariable('title',_("Group"));
 
 $tpl->show();
 
-function getRightLevelBox($right_id, $rights = array()) 
-{
-    global $level;
-    $right = (int) hasGroupRight($right_id, $rights);
-    if ($level>1){
-        $select = '<select name="rights['.$right_id.']">';
-        for($i=0;$i<4;$i++){
-            if ($i==$right) {
-                $select .= '<option value="'.$i.'" selected="selected">'.constant('HRADMIN_LEVEL_'.$i).'</option>';
-            } else {
-                $select .= '<option value="'.$i.'">'.constant('HRADMIN_LEVEL_'.$i).'</option>';
-            }
-        }
-        $select .= '</select>';
-        return $select;
-    } else {
-        return  constant('HRADMIN_LEVEL_'.$right);  
-    }
-    
-}
+
+        
 
 function hasGroupRight($right_id,$rights = array())  
 {
-    return $rights[$right_id];
+    return ; #$rights[$right_id];
 }
 
 function setGroupRights($group_id,$newRights = array()) {
     global $admin;
-    $rights = getGroupRight($group_id);
+    
+    
+    $params = array('fields'  => array('right_id','right_level'),
+                    'filters' => array('group_id' => $group_id));
+                            
+    $rights = $admin->perm->getRights($params);
+    
+    
+print "<h1>Group=$group_id</h1>";
+var_dump::display($rights);
+    
     foreach ($rights as $right => $level) {
-        $objRightsAdminPerm->revokeGroupRight($group_id,$right);
+    
+        $filters  = array('right_id' => $right,
+                          'group_id' => $group_id);
+
+        $removed = $admin->perm->revokeGroupRight($filters);
+    
+    
     }
     if (!empty($newRights)) {
         foreach ($newRights as $newRight => $level) {
             if ((int)$level > 0) {                
-                $objRightsAdminPerm->grantGroupRight($group_id,$newRight);
-                $objRightsAdminPerm->updateGroupRight($group_id,$newRight,$level);
+            
+                $data = array('right_level' => $level);
+                $filters = array('right_id' => $newRight,
+                                 'group_id' => $group_id);
+  
+                $updated = $admin->perm->updateGroupRight($data, $filters);
             }
         }    
     }
